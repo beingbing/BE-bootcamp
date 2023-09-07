684026c756f201d20975ca08ba9efe03d654b500
Samar Shaikh <samar.shaikh@bharatpe.com>
2023-09-08 03:22:36 +0530

    10 aug 2023 | lecture 1 | hello world

---
+++ src/Lecture-1/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+
+    // public static void class main(String[] args) { // compile time error on class keyword
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
