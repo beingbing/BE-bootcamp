3acea8d2a40ec1ea8196bf5389809378baa3eca7
Samar Shaikh <samar.shaikh@bharatpe.com>
2023-09-09 18:38:33 +0530

    10 aug 2023 | lecture 4 | string and string builder

---
+++ src/Lecture-4/Practice.java
@@ -0,0 +1,11 @@
+public class Practice {
+    public static void main(String args[]) {
+        /*
+         * 1. convert a decimal number into binary form
+         * */
+
+        /*
+         * 2. Take string as an input and check if it is a palindrome.
+         * */
+    }
+}
