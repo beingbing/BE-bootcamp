26afc26af66f0fc4c067476c96fc57dff2f8981e
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2023-12-08 10:08:42 +0530

    16 Sep 2023 | lecture 15 | staring setup

---
+++ src/Lecture-15/bootcamp/src/main/java/com/samar/bootcamp/BootcampApplication.java
@@ -0,0 +1,17 @@
+package com.samar.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class BootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampApplication.class, args);
+		System.out.println("Hello Samar !!!");
+
+		ApplicationContext ctx
+	}
+
+}
