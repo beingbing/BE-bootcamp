b06ff6b743055390d62b6170976601435ba8ec05
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2023-12-09 23:45:51 +0530

    17 Sep 2023 | lecture 16 | assignment 2 | complete to-do list with Thymeleaf

--- src/Lecture-16/bootcamp/src/main/java/com/samar/bootcamp/todo/controller/ThymeleafController.java
+++ src/Lecture-16/bootcamp/src/main/java/com/samar/bootcamp/todo/controller/ThymeleafController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class ThymeleafController {
@@ -32,8 +30,44 @@ public class ThymeleafController {
         if (ObjectUtils.isEmpty(newItem.getContent())) return "index";
         System.out.println("add item: " + newItem);
         toDoManager.addNewTodoItem(newItem);
-        model.addAttribute("items", toDoManager.getTodoList().getItem());
-        model.addAttribute("newItem", new ToDoList.ToDoItem());
-        return "index";
+        return "redirect:/";
+    }
+
+    @PostMapping("/update")
+    public String updateItem(@ModelAttribute("newItem") ToDoList.ToDoItem newItem, Model model) {
+        if (ObjectUtils.isEmpty(newItem.getContent())) return "index";
+        System.out.println("update item: " + newItem);
+        toDoManager.updateSpecificToDoItem(newItem.getItemId(), newItem);
+        return "redirect:/";
+    }
+
+    @PostMapping("/toggle/{id}")
+    public String updateItem(@PathVariable Long id, Model model) {
+        System.out.println("update item: " + id);
+        ToDoList.ToDoItem item = toDoManager.getToDoItemFromList(id);
+        item.setIsCompleted(!item.getIsCompleted());
+        toDoManager.updateSpecificToDoItem(id, item);
+        return "redirect:/";
+    }
+
+    /*
+    * GET method can directly be called from link-expression
+    * but to call a POST method, we will need a <form> tag.
+    * Similar to what is done for /toggle
+    * */
+    @GetMapping("/delete/{id}")
+    public String deleteItem(@PathVariable Long id, Model model) {
+        System.out.println("delete item: " + id);
+        toDoManager.deleteSpecificTodoItem(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editItem(@PathVariable Long id, Model model) {
+        System.out.println("edit item id: " + id);
+        ToDoList.ToDoItem item = toDoManager.getToDoItemFromList(id);
+        System.out.println("edit item: " + item);
+        model.addAttribute("item", item);
+        return "update";
     }
 }
