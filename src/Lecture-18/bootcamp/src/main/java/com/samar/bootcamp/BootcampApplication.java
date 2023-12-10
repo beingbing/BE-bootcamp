78624af638fc8b05ecb4cb3c2016e02e05d8a788
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2023-12-10 13:33:03 +0530

    24 Sep 2023 | lecture 18 | example of threads scheduling and concurrency

--- src/Lecture-18/bootcamp/src/main/java/com/samar/bootcamp/BootcampApplication.java
+++ src/Lecture-18/bootcamp/src/main/java/com/samar/bootcamp/BootcampApplication.java
@@ -2,22 +2,62 @@ package com.samar.bootcamp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BootcampApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BootcampApplication.class, args);
-		System.out.println("Hello Samar !!!");
+    public static void main(String[] args) {
+        SpringApplication.run(BootcampApplication.class, args);
+        System.out.println("Hello Samar !!!");
 
-		// Thread: tell this guy what to run
-		// Runnable: It's a simple interface in java, which has a run()
+        // Thread: tell this guy what to run
+        // Runnable: It's a simple interface in java, which has a run()
 
-		Thread t1 = new Thread(new MyRunnable("first"));
-		t1.start(); // t1 is launched and now we move down
-		// now, JVM will call its run() to start execution
-		System.out.println("main thread in execution");
-	}
+        Thread t1 = new Thread(new MyRunnable("first"));
+        Thread t2 = new Thread(new MyRunnable("Second"));
+        Thread t3 = new Thread(new MyRunnable("Third"));
+        t1.start(); // t1 is launched and now we move down
+        // now, JVM will call its run() to start execution
+        t2.start();
+        t3.start();
+        System.out.println("main thread in execution");
+    }
+
+	/*
+    * You can see the difference in order of logs printed and the order
+    * in which they were written -
+    * Hello Samar !!!
+    * first
+    * Second
+    * Second
+    * Second
+    * Second
+    * Second
+    * Second
+    * Second
+    * Second
+    * Second
+    * Second
+    * main thread in execution
+    * first
+    * first
+    * first
+    * first
+    * first
+    * first
+    * first
+    * first
+    * first
+    * Third
+    * Third
+    * Third
+    * Third
+    * Third
+    * Third
+    * Third
+    * Third
+    * Third
+    * Third
+    */
 
 }
