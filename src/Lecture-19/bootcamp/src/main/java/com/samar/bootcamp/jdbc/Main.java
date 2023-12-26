ae9b49519bc6783f018bf7f643bda11ef6eaf0fd
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2023-12-26 17:18:22 +0530

    30 Sep 2023 | lecture 19 | closing objects of connection after DB quering is done

--- src/Lecture-19/bootcamp/src/main/java/com/samar/bootcamp/jdbc/Main.java
+++ src/Lecture-19/bootcamp/src/main/java/com/samar/bootcamp/jdbc/Main.java
@@ -9,6 +9,7 @@ import java.sql.Statement;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
+        /**
         Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:3306/samar");
         // connection url can be found in documentation
         // localhost:<port>/<DB-name>
@@ -33,6 +34,34 @@ public class Main {
         ResultSet rs3 = stat3.executeQuery("select * from profiles");
         while (rs3.next()) {
             System.out.println(rs3.getInt("id"));
+        }
+        */
+    }
+
+    private static void connectUsingDriverManager(String url, String userName, String password, String tableName, String colName) throws SQLException {
+        Connection c = null;
+        try {
+            c = DriverManager.getConnection(url, userName, password);
+            Statement stm = c.createStatement();
+            ResultSet rs = stm.executeQuery("select * from " + tableName);
+            while (rs.next()) {
+                System.out.println(rs.getString(colName));
+            }
+            rs.close();
+            stm.close();
+            // it is important to close() to prevent memory leak
+            // Q.   when a non-pointed object is automatically cleared by garbage collector, then
+            //      what's the use of manually close().
+            // A.   1. Garbage collector doesn't run instantly. It runs after some periodic interval,
+            //      and, that interval can be large as well. So, to instantly scrap these humongous
+            //      object once they are not needed, we do .close().
+
+            // A quick way to do is to scrap Connection Object and all the Objects born out of it
+            // will be destroyed instantly.
+        } finally {
+            if (c != null) {
+                c.close();
+            }
         }
     }
 }
