9bf13357ce315f1dbbbe4dee41438b67ebdb6353
Samar Shaikh <samar.shaikh@bharatpe.com>
2024-02-17 14:45:37 +0530

    01 Oct 2023 | lecture 20 | ACID properties

---
+++ src/Lecture-20/bootcamp/src/main/java/com/samar/bootcamp/ACID.java
@@ -0,0 +1,11 @@
+package com.samar.bootcamp;
+
+public class ACID {
+    /*
+     * A: Atomicity:    All or nothing: either complete it or do not even start
+     * C: Consistency:  measurement of correctness: total of a txn before and after should be same
+     * I: Isolation:    an ongoing transaction should not know/affect other transactions
+     * D: Durability:   after commit, data should be persisted and permanent
+     *
+     */
+}
